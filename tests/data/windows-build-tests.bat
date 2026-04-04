5a0b33d75ad86384a766707d0200830c4726877d
nilsnolde <nilsnolde@proton.me>
2026-04-04 11:36:39 +0200

    enable win tests

--- tests/data/windows-build-tests.bat
+++ tests/data/windows-build-tests.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
-SETLOCAL
-SET EL=0
+SETLOCAL EnableDelayedExpansion
 
 SET DATA_DIR=%CD%
 
@@ -9,8 +8,9 @@ SET test_region_ch=ch\monaco
 SET test_region_mld=mld\monaco
 SET test_osm=%test_region%.osm.pbf
 
-python -m osrm extract -p %DATA_DIR%\profiles\car.lua %DATA_DIR%\monaco.osm.pbf
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+SET CMD=python -m osrm extract -p %DATA_DIR%\profiles\car.lua %DATA_DIR%\monaco.osm.pbf
+%CMD%
+IF !ERRORLEVEL! NEQ 0 (SET EL=!ERRORLEVEL! & GOTO ERROR)
 
 MKDIR ch
 XCOPY %test_region%.osrm.* ch\
@@ -19,21 +19,24 @@ MKDIR mld
 XCOPY %test_region%.osrm.* mld\
 XCOPY %test_region%.osrm mld\
 
-python -m osrm contract %test_region_ch%.osrm
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+SET CMD=python -m osrm contract %test_region_ch%.osrm
+%CMD%
+IF !ERRORLEVEL! NEQ 0 (SET EL=!ERRORLEVEL! & GOTO ERROR)
 
-python -m osrm partition %test_region_mld%.osrm
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+SET CMD=python -m osrm partition %test_region_mld%.osrm
+%CMD%
+IF !ERRORLEVEL! NEQ 0 (SET EL=!ERRORLEVEL! & GOTO ERROR)
 
-python -m osrm customize %test_region_mld%.osrm
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+SET CMD=python -m osrm customize %test_region_mld%.osrm
+%CMD%
+IF !ERRORLEVEL! NEQ 0 (SET EL=!ERRORLEVEL! & GOTO ERROR)
 
 GOTO DONE
 
 :ERROR
 ECHO ~~~~~~~~~~~~~~~~~~~~~~ ERROR %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ECHO ERRORLEVEL^: %ERRORLEVEL%
-SET EL=%ERRORLEVEL%
+ECHO Failed command: %CMD%
+ECHO Exit code: %EL%
 
 :DONE
 ECHO ~~~~~~~~~~~~~~~~~~~~~~ DONE %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
